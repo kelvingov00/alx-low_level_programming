ba92f16d6436badebb97e3d83338e7cacbfe3bf8
kelvingov00 <kelvingovender00@gmail.com>
2022-09-13 00:54:50 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ * Description - print alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet - 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
