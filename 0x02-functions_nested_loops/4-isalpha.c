3b89b16d20ab5145420e838a86fbd44ee955e40d
kelvingov00 <kelvingovender00@gmail.com>
2022-09-13 01:14:47 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - return 1 if c is a letter
+ * Description -  checks for alphabetic character
+ * @c: the int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
