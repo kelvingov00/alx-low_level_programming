57164a034d08b8b72c53b637aefde4ed5a805076
kelvingov00 <kelvingovender00@gmail.com>
2022-09-14 00:05:56 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,8 +2,7 @@
 
 /**
  * main - causes an infinite loop
- *
- * Return: 0.
+ * Return: 0
  */
 
 int main(void)
@@ -13,7 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
