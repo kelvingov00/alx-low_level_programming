6d4213abf9e8fe6049bbac01d43ab9d294be0f11
kelvingov00 <kelvingovender00@gmail.com>
2022-09-22 15:03:53 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - takes an int and prints it
+ * @n: int to print
+ */
+
+void print_number(int n)
+{
+	int count, y, k;
+
+	y = 1000000000;
+	if (n == 0)
+		_putchar('0');
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	for (count = 0; count < 10; count++, y /= 10)
+	{
+		if (n / y == 0)
+			continue;
+		else
+		{
+			k = (-(n / y) % 10);
+			if (k < 0)
+				k *= -1;
+			_putchar(k + '0');
+		}
+	}
+}
