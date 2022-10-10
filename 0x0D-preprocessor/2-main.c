35321c786450dc8c934d6029c33a7930c2044a35
kelvingov00 <kelvingovender00@gmail.com>
2022-10-10 13:15:14 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file
+ *
+ * Description: a program that prints the name
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
